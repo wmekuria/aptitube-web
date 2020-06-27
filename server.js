a18ef40381633aba63a2b95526e090f9da800866
Workneh Mekuria <workneh.mekuria@gmail.com>
2020-06-27 14:35:49 -0500

    update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/build'));
 app.listen(PORT);
